b3317894a5b5db091dec8d8c7598eff3e48bc05d
egavilan1987 <enmanuelgc@gmail.com>
2019-06-18 12:17:35 -0400

    Create egmedia_contacts.sql

---
+++ db/egmedia_contacts.sql
@@ -0,0 +1,17 @@
+egmedia_contacts.sql
+
+CREATE DATABASE egmedia_contacts;
+
+USE egmedia_contacts;
+
+CREATE TABLE tbl_contacts(
+     contact_id(11) NOT NULL, AUTO_INCREMENT,
+     firstname VARCHAR(20) NOT NULL,
+     lastname VARCHAR(20) NOT NULL,
+     email VARCHAR (50) NOT NULL, 
+     phone VARCHAR (20) NOT NULL,
+     subject VARCHAR (20) NOT NULL,
+     message VARCHAR (200) NOT NULL,
+     sentDate DATETIME NOT NOT NULL
+     PRIMARY KEY(contact_id)
+);
